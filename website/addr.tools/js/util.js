fb442d548907c95a95a8e03547553f374a4156cd
root <165865819+brianshea2@users.noreply.github.com>
2024-10-23 14:49:08 +0000

    add HTTPError

--- website/addr.tools/js/util.js
+++ website/addr.tools/js/util.js
@@ -1,12 +1,19 @@
+class HTTPError extends Error {
+  constructor(code) {
+    super(`Received HTTP status code ${code}`)
+    this.code = code
+    this.name = 'HTTPError'
+  }
+}
 function encode(str) {
   return str?.replace(/["&<>]/g, match => `&#${match.charCodeAt(0)};`)
 }
 function fetchOk(resource, opts) {
-  return fetch(resource, Object.assign({ referrerPolicy: "no-referrer" }, opts)).then(response => {
+  return fetch(resource, Object.assign({ referrerPolicy: 'no-referrer' }, opts)).then(response => {
     if (!response.ok) {
-      throw new Error(`Received HTTP status code ${response.status}`)
+      throw new HTTPError(response.status)
     }
     return response
   })
 }
-export { encode, fetchOk }
+export { HTTPError, encode, fetchOk }
