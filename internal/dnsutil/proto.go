c5035a16b9ec9d34af8f2d971ae5f9e6cde41239
root <165865819+brianshea2@users.noreply.github.com>
2025-05-29 14:20:12 +0000

    use constants, only call ConnectionState if necessary

--- internal/dnsutil/proto.go
+++ internal/dnsutil/proto.go
@@ -7,22 +7,34 @@ import (
 	"github.com/miekg/dns"
 )
 
+const (
+	ProtoUDP = "UDP"
+	ProtoTCP = "TCP"
+	ProtoTLS = "TLS"
+)
+
 func GetAddrProtocol(addr net.Addr, cstate *tls.ConnectionState) string {
 	switch addr.Network() {
 	case "udp":
-		return "UDP"
+		return ProtoUDP
 	case "tcp":
 		if cstate != nil {
-			return "TLS"
+			return ProtoTLS
 		}
-		return "TCP"
+		return ProtoTCP
 	}
 	return ""
 }
 
 func GetWriterProtocol(w dns.ResponseWriter) string {
-	return GetAddrProtocol(
-		w.LocalAddr(),
-		w.(dns.ConnectionStater).ConnectionState(),
-	)
+	switch w.LocalAddr().Network() {
+	case "udp":
+		return ProtoUDP
+	case "tcp":
+		if w.(dns.ConnectionStater).ConnectionState() != nil {
+			return ProtoTLS
+		}
+		return ProtoTCP
+	}
+	return ""
 }
