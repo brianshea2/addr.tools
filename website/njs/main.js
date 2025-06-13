5cb4fd7f674eaf6e922512241e7456cbb0601dae
root <165865819+brianshea2@users.noreply.github.com>
2025-06-13 20:25:00 +0000

    header-echo updates

--- website/njs/main.js
+++ website/njs/main.js
@@ -1,7 +1,10 @@
-const get_self_ip_host = r =>
-  r.remoteAddress.replace(/[.:]/g, "-").replace(/^-/, "0-").replace(/-$/, "-0") + ".ip.addr.tools"
+const get_self_ip_host = r => r.remoteAddress.replace(/[.:]/g, "-").replace(/^-/, "0-").replace(/-$/, "-0") + ".ip.addr.tools"
 
 const header_echo_content = r => {
+  if (r.args["status-code"] === "204" || r.args["status-code"] === "205" || r.args["status-code"] === "304") {
+    r.return(+r.args["status-code"])
+    return
+  }
   let content = `# IP: ${r.variables.remote_addr}.${r.variables.remote_port} > ${r.variables.server_addr}.${r.variables.server_port}\n`
   if (r.variables.ssl_protocol) {
     content += `# TLS: ${r.variables.ssl_session_reused === "r" ? "Reused" : "New"}, ${r.variables.ssl_protocol}\n`
@@ -20,42 +23,47 @@ const header_echo_content = r => {
   r.return(200, content)
 }
 
+const disallowedHeaderNames = /^(Connection|Content-Length|Transfer-Encoding)$/i
+const validHeaderName = /^[a-zA-Z0-9_-]+$/
+const validHeaderValue = /^[ -~]+$/
+const validStatusCode = /^[2-5][0-9][0-9]$/
 const header_echo = r => {
-  const validHeaderName = /^[a-zA-Z0-9_-]+$/
-  const validHeaderValue = /^[ -~]+$/
-  const validStatusCode = /^[1-5][0-9][0-9]$/
-  let statusCode = 200
   const headersOut = {}
   for (const key in r.args) {
-    if (validHeaderName.test(key)) {
-      const name = key.toLowerCase()
-      const values = Array.isArray(r.args[key]) ? r.args[key] : [r.args[key]]
-      for (let i = 0; i < values.length; i++) {
-        if (name === "status-code") {
-          if (validStatusCode.test(values[i])) {
-            statusCode = +values[i]
-          }
-        } else if (validHeaderValue.test(values[i])) {
-          if (headersOut[name] === undefined) {
-            headersOut[name] = values[i]
-          } else if (Array.isArray(headersOut[name])) {
-            headersOut[name].push(values[i])
-          } else {
-            headersOut[name] = [headersOut[name], values[i]]
-          }
-        }
+    if (key === "status-code") {
+      continue
+    }
+    if (disallowedHeaderNames.test(key)) {
+      continue
+    }
+    if (!validHeaderName.test(key)) {
+      continue
+    }
+    const name = key.toLowerCase()
+    const values = Array.isArray(r.args[key]) ? r.args[key] : [r.args[key]]
+    for (let i = 0; i < values.length; i++) {
+      if (!validHeaderValue.test(values[i])) {
+        continue
+      }
+      if (headersOut[name] === undefined) {
+        headersOut[name] = values[i]
+      } else if (Array.isArray(headersOut[name])) {
+        headersOut[name].push(values[i])
+      } else {
+        headersOut[name] = [headersOut[name], values[i]]
       }
     }
   }
-  r.status = statusCode
-  r.headersOut["Content-Type"] = "text/plain"
   for (const key in headersOut) {
     r.headersOut[key] = headersOut[key]
+  }
+  if (r.status === 200 && typeof r.args["status-code"] === "string" && validStatusCode.test(r.args["status-code"])) {
+    r.status = +r.args["status-code"]
   }
 }
 
 export default {
   get_self_ip_host,
   header_echo_content,
-  header_echo
+  header_echo,
 }
