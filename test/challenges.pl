d78898125d37b6eec2ad2d5e5cd430152668829a
root <165865819+brianshea2@users.noreply.github.com>
2025-09-06 20:43:16 +0000

    remove deprecated api tests

--- test/challenges.pl
+++ test/challenges.pl
@@ -8,79 +8,62 @@ use URI::Escape;
 sub curl        { print '.'; sleep 1; my $s = shift; `curl -s 'https://challenges.addr.tools/$s' @_` // '' }
 sub dig         { `dig \@127.0.0.1 +short txt @_` // '' }
 sub domain      { sha224_hex($_[0]) . '.challenges.addr.tools.' }
-sub secret      { 'test0-9A-Za-z=_-' . $_[0] }
-sub secret2     { 'a=1&b=2; !@#$%^&*()/+' . $_[0] }
+sub secret      { 'a=1&b=2; !@#$%^&*()/+' . $_[0] }
 sub withNewline { "$_[0]\n" }
 sub expect      { $_[0] eq $_[1] or warn "\nFAILED: line " . (caller)[2] . "; got: $_[0]; expected: $_[1]\n" }
 
-# /<secret> GET
-expect( curl(secret(1)),                                        withNewline(domain(secret(1)))      );
-expect( dig(domain(secret(1))),                                 ''                                  );
-expect( curl(secret(1) . '?txt=123ABCabc_-'),                   withNewline('OK')                   );
-expect( dig(domain(secret(1))),                                 withNewline('"123ABCabc_-"')        );
-expect( curl(secret(1) . '?txt=123ABCabc_-', '-X', 'DELETE'),   ''                                  );
-expect( dig(domain(secret(1))),                                 ''                                  );
-
-# /<secret> POST
-expect( curl(secret(2)),                                        withNewline(domain(secret(2)))      );
-expect( dig(domain(secret(2))),                                 ''                                  );
-expect( curl(secret(2), '-d', '123ABCabc_-'),                   withNewline('OK')                   );
-expect( dig(domain(secret(2))),                                 withNewline('"123ABCabc_-"')        );
-expect( curl(secret(2), '-d', '123ABCabc_-', '-X', 'DELETE'),   ''                                  );
-expect( dig(domain(secret(2))),                                 ''                                  );
-
 # /?secret=<secret> GET
-my $q = '?secret=' . uri_escape(secret2(3));
-expect( curl($q),                                               withNewline(domain(secret2(3)))     );
-expect( dig(domain(secret2(3))),                                ''                                  );
+my $q = '?secret=' . uri_escape(secret(1));
+expect( curl($q),                                               withNewline(domain(secret(1)))      );
+expect( dig(domain(secret(1))),                                 ''                                  );
 expect( curl("$q&txt=123ABCabc_-"),                             withNewline('OK')                   );
-expect( dig(domain(secret2(3))),                                withNewline('"123ABCabc_-"')        );
+expect( dig(domain(secret(1))),                                 withNewline('"123ABCabc_-"')        );
 expect( curl("$q&txt=123ABCabc_-", '-X', 'DELETE'),             ''                                  );
-expect( dig(domain(secret2(3))),                                ''                                  );
+expect( dig(domain(secret(1))),                                 ''                                  );
 
 # /?secret=<secret> POST
-$q = '?secret=' . uri_escape(secret2(4));
-expect( curl($q),                                               withNewline(domain(secret2(4)))     );
-expect( dig(domain(secret2(4))),                                ''                                  );
+$q = '?secret=' . uri_escape(secret(2));
+expect( curl($q),                                               withNewline(domain(secret(2)))      );
+expect( dig(domain(secret(2))),                                 ''                                  );
 expect( curl($q, '-d', '123ABCabc_-'),                          withNewline('OK')                   );
-expect( dig(domain(secret2(4))),                                withNewline('"123ABCabc_-"')        );
+expect( dig(domain(secret(2))),                                 withNewline('"123ABCabc_-"')        );
 expect( curl($q, '-d', '123ABCabc_-', '-X', 'DELETE'),          ''                                  );
-expect( dig(domain(secret2(4))),                                ''                                  );
+expect( dig(domain(secret(2))),                                 ''                                  );
 
 # POST only
-my $d = "--data-urlencode 'secret=" . secret2(5) . "'";
-expect( curl('', $d),                                           withNewline(domain(secret2(5)))     );
-expect( dig(domain(secret2(5))),                                ''                                  );
+my $d = "--data-urlencode 'secret=" . secret(3) . "'";
+expect( curl('', $d),                                           withNewline(domain(secret(3)))      );
+expect( dig(domain(secret(3))),                                 ''                                  );
 expect( curl('', $d, '-d', 'txt=123ABCabc_-'),                  withNewline('OK')                   );
-expect( dig(domain(secret2(5))),                                withNewline('"123ABCabc_-"')        );
+expect( dig(domain(secret(3))),                                 withNewline('"123ABCabc_-"')        );
 expect( curl('', $d, '-d', 'txt=123ABCabc_-', '-X', 'DELETE'),  ''                                  );
-expect( dig(domain(secret2(5))),                                ''                                  );
+expect( dig(domain(secret(3))),                                 ''                                  );
 
 # POST with same GET
-$q = '?secret=' . uri_escape(secret2(6));
-$d = "--data-urlencode 'secret=" . secret2(6) . "'";
-expect( curl($q, $d),                                                               withNewline(domain(secret2(6))) );
-expect( dig(domain(secret2(6))),                                                    ''                              );
+$q = '?secret=' . uri_escape(secret(4));
+$d = "--data-urlencode 'secret=" . secret(4) . "'";
+expect( curl($q, $d),                                                               withNewline(domain(secret(4)))  );
+expect( dig(domain(secret(4))),                                                     ''                              );
 expect( curl("$q&txt=123ABCabc_-", $d, '-d', 'txt=123ABCabc_-'),                    withNewline('OK')               );
-expect( dig(domain(secret2(6))),                                                    withNewline('"123ABCabc_-"')    );
+expect( dig(domain(secret(4))),                                                     withNewline('"123ABCabc_-"')    );
 expect( curl("$q&txt=123ABCabc_-", $d, '-d', 'txt=123ABCabc_-', '-X', 'DELETE'),    ''                              );
-expect( dig(domain(secret2(6))),                                                    ''                              );
+expect( dig(domain(secret(4))),                                                     ''                              );
 
 # POST with different GET secret
-$q = '?secret=' . uri_escape(secret2(7));
-$d = "--data-urlencode 'secret=" . secret2(8) . "'";
+$q = '?secret=' . uri_escape(secret(5));
+$d = "--data-urlencode 'secret=" . secret(6) . "'";
 expect( curl($q, $d),                                                               withNewline('multiple values found for "secret"') );
 expect( curl("$q&txt=123ABCabc_-", $d, '-d', 'txt=123ABCabc_-'),                    withNewline('multiple values found for "secret"') );
 expect( curl("$q&txt=123ABCabc_-", $d, '-d', 'txt=123ABCabc_-', '-X', 'DELETE'),    withNewline('multiple values found for "secret"') );
 
 # POST with different GET txt
-$q = '?secret=' . uri_escape(secret2(9));
-$d = "--data-urlencode 'secret=" . secret2(9) . "'";
+$q = '?secret=' . uri_escape(secret(7));
+$d = "--data-urlencode 'secret=" . secret(7) . "'";
 expect( curl("$q&txt=123ABCabc_-", $d, '-d', 'txt=somethingelse'),                  withNewline('multiple values found for "txt"') );
 
 # invalid txt
-$q = '?secret=' . uri_escape(secret2(10));
-$d = "--data-urlencode 'secret=" . secret2(10) . "'";
+$q = '?secret=' . uri_escape(secret(8));
+$d = "--data-urlencode 'secret=" . secret(8) . "'";
 expect( curl("$q&txt=invalid.value"),                           withNewline('invalid value for "txt"')  );
 expect( curl($q, '-d', 'txt=invalid.value'),                    withNewline('invalid value for "txt"')  );
 expect( curl('', $d, '-d', 'txt=invalid.value'),                withNewline('invalid value for "txt"')  );
--- test/dyn.pl
+++ test/dyn.pl
@@ -8,96 +8,71 @@ use URI::Escape;
 sub curl        { print '.'; sleep 1; my $s = shift; `curl -s 'https://dyn.addr.tools/$s' @_` // '' }
 sub dig         { `dig \@127.0.0.1 +short @_` // '' }
 sub domain      { sha224_hex($_[0]) . '.dyn.addr.tools.' }
-sub secret      { 'test0-9A-Za-z=_-' . $_[0] }
-sub secret2     { 'a=1&b=2; !@#$%^&*()/+' . $_[0] }
+sub secret      { 'a=1&b=2; !@#$%^&*()/+' . $_[0] }
 sub withNewline { "$_[0]\n" }
 sub expect      { $_[0] eq $_[1] or warn "\nFAILED: line " . (caller)[2] . "; got: $_[0]; expected: $_[1]\n" }
 
-# /<secret> GET
-expect( curl(secret(1)),                                        withNewline(domain(secret(1)))      );
-expect( dig(domain(secret(1))),                                 ''                                  );
-expect( curl(secret(1) . '?ip=192.0.2.101'),                    withNewline('OK')                   );
-expect( dig(domain(secret(1))),                                 withNewline('192.0.2.101')          );
-expect( curl(secret(1), '-X', 'DELETE'),                        ''                                  );
-expect( dig(domain(secret(1))),                                 ''                                  );
-
-# /<secret> POST
-expect( curl(secret(2)),                                        withNewline(domain(secret(2)))      );
-expect( dig(domain(secret(2))),                                 ''                                  );
-expect( curl(secret(2), '-d', '192.0.2.101'),                   withNewline('OK')                   );
-expect( dig(domain(secret(2))),                                 withNewline('192.0.2.101')          );
-expect( curl(secret(2), '-X', 'DELETE'),                        ''                                  );
-expect( dig(domain(secret(2))),                                 ''                                  );
-
-# /<secret> PUT
-expect( curl(secret(3)),                                        withNewline(domain(secret(3)))      );
-expect( dig(domain(secret(3))),                                 ''                                  );
-expect( curl(secret(3), '-d', '192.0.2.101', '-X', 'PUT'),      withNewline('OK')                   );
-expect( dig(domain(secret(3))),                                 withNewline('192.0.2.101')          );
-expect( curl(secret(3), '-X', 'DELETE'),                        ''                                  );
-expect( dig(domain(secret(3))),                                 ''                                  );
-
 # /?secret=<secret> GET
-my $q = '?secret=' . uri_escape(secret2(4));
-expect( curl($q),                                               withNewline(domain(secret2(4)))     );
-expect( dig(domain(secret2(4))),                                ''                                  );
+my $q = '?secret=' . uri_escape(secret(1));
+expect( curl($q),                                               withNewline(domain(secret(1)))      );
+expect( dig(domain(secret(1))),                                 ''                                  );
 expect( curl("$q&ip=192.0.2.101"),                              withNewline('OK')                   );
-expect( dig(domain(secret2(4))),                                withNewline('192.0.2.101')          );
+expect( dig(domain(secret(1))),                                 withNewline('192.0.2.101')          );
 expect( curl($q, '-X', 'DELETE'),                               ''                                  );
-expect( dig(domain(secret2(4))),                                ''                                  );
+expect( dig(domain(secret(1))),                                 ''                                  );
 
 # /?secret=<secret> POST
-$q = '?secret=' . uri_escape(secret2(5));
-expect( curl($q),                                               withNewline(domain(secret2(5)))     );
-expect( dig(domain(secret2(5))),                                ''                                  );
+$q = '?secret=' . uri_escape(secret(2));
+expect( curl($q),                                               withNewline(domain(secret(2)))      );
+expect( dig(domain(secret(2))),                                 ''                                  );
 expect( curl($q, '-d', '192.0.2.101'),                          withNewline('OK')                   );
-expect( dig(domain(secret2(5))),                                withNewline('192.0.2.101')          );
+expect( dig(domain(secret(2))),                                 withNewline('192.0.2.101')          );
 expect( curl($q, '-X', 'DELETE'),                               ''                                  );
-expect( dig(domain(secret2(5))),                                ''                                  );
+expect( dig(domain(secret(2))),                                 ''                                  );
 
 # POST only
-my $d = "--data-urlencode 'secret=" . secret2(6) . "'";
-expect( curl('', $d),                                           withNewline(domain(secret2(6)))     );
-expect( dig(domain(secret2(6))),                                ''                                  );
+my $d = "--data-urlencode 'secret=" . secret(3) . "'";
+expect( curl('', $d),                                           withNewline(domain(secret(3)))      );
+expect( dig(domain(secret(3))),                                 ''                                  );
 expect( curl('', $d, '-d', 'ip=192.0.2.101'),                   withNewline('OK')                   );
-expect( dig(domain(secret2(6))),                                withNewline('192.0.2.101')          );
+expect( dig(domain(secret(3))),                                 withNewline('192.0.2.101')          );
 expect( curl('', $d, '-X', 'DELETE'),                           ''                                  );
-expect( dig(domain(secret2(6))),                                ''                                  );
+expect( dig(domain(secret(3))),                                 ''                                  );
 
 # PUT only
-$d = "--data-urlencode 'secret=" . secret2(7) . "'";
-expect( curl('', $d, '-X', 'PUT'),                              withNewline(domain(secret2(7)))     );
-expect( dig(domain(secret2(7))),                                ''                                  );
+$d = "--data-urlencode 'secret=" . secret(4) . "'";
+expect( curl('', $d, '-X', 'PUT'),                              withNewline(domain(secret(4)))      );
+expect( dig(domain(secret(4))),                                 ''                                  );
 expect( curl('', $d, '-X', 'PUT', '-d', 'ip=192.0.2.101'),      withNewline('OK')                   );
-expect( dig(domain(secret2(7))),                                withNewline('192.0.2.101')          );
+expect( dig(domain(secret(4))),                                 withNewline('192.0.2.101')          );
 expect( curl('', $d, '-X', 'DELETE'),                           ''                                  );
-expect( dig(domain(secret2(7))),                                ''                                  );
+expect( dig(domain(secret(4))),                                 ''                                  );
 
 # POST with same GET
-$q = '?secret=' . uri_escape(secret2(8));
-$d = "--data-urlencode 'secret=" . secret2(8) . "'";
-expect( curl($q, $d),                                           withNewline(domain(secret2(8)))     );
-expect( dig(domain(secret2(8))),                                ''                                  );
+$q = '?secret=' . uri_escape(secret(5));
+$d = "--data-urlencode 'secret=" . secret(5) . "'";
+expect( curl($q, $d),                                           withNewline(domain(secret(5)))      );
+expect( dig(domain(secret(5))),                                 ''                                  );
 expect( curl("$q&ip=192.0.2.101", $d, '-d', 'ip=192.0.2.101'),  withNewline('OK')                   );
-expect( dig(domain(secret2(8))),                                withNewline('192.0.2.101')          );
+expect( dig(domain(secret(5))),                                 withNewline('192.0.2.101')          );
 expect( curl($q, $d, '-X', 'DELETE'),                           ''                                  );
-expect( dig(domain(secret2(8))),                                ''                                  );
+expect( dig(domain(secret(5))),                                 ''                                  );
 
 # POST with different GET secret
-$q = '?secret=' . uri_escape(secret2(9));
-$d = "--data-urlencode 'secret=" . secret2(10) . "'";
+$q = '?secret=' . uri_escape(secret(6));
+$d = "--data-urlencode 'secret=" . secret(7) . "'";
 expect( curl($q, $d),                                           withNewline('multiple values found for "secret"') );
 expect( curl("$q&ip=192.0.2.101", $d, '-d', 'ip=192.0.2.101'),  withNewline('multiple values found for "secret"') );
 expect( curl($q, $d, '-X', 'DELETE'),                           withNewline('multiple values found for "secret"') );
 
 # POST with different GET ip
-$q = '?secret=' . uri_escape(secret2(11));
-$d = "--data-urlencode 'secret=" . secret2(11) . "'";
+$q = '?secret=' . uri_escape(secret(8));
+$d = "--data-urlencode 'secret=" . secret(8) . "'";
 expect( curl("$q&ip=192.0.2.101", $d, '-d', 'ip=192.0.2.202'),  withNewline('multiple values found for "ip"') );
 
 # invalid IP
-$q = '?secret=' . uri_escape(secret2(12));
-$d = "--data-urlencode 'secret=" . secret2(12) . "'";
+$q = '?secret=' . uri_escape(secret(9));
+$d = "--data-urlencode 'secret=" . secret(9) . "'";
 expect( curl("$q&ip=192.0.2.abc"),                              withNewline('invalid value for "ip"')   );
 expect( curl($q, '-d', 'ip=192.0.2.abc'),                       withNewline('invalid value for "ip"')   );
 expect( curl('', $d, '-d', 'ip=192.0.2.abc'),                   withNewline('invalid value for "ip"')   );
